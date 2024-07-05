b83e2c45f7d9f26f5e2d23e334505e145b2775ac
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-05 15:15:01 +0500

    Vercel Config

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 router.post('/register', async (req, res) => {
-    const newUser = new User(req.body)
+    const newUser = new User(req.body);
     try {
-        const user = await newUser.save()
-        res.send('User Registered Successfully!')
+        const user = await newUser.save();
+        res.send('User Registered Successfully!');
     } catch (error) { return res.status(400).json({ error }) }
 })
 
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     try {
-        const user = await User.findOne({ email: email, password: password })
+        const user = await User.findOne({ email: email, password: password });
         if (user) {
             const temp = {
                 name: user.name,
                 email: user.email,
                 isAdmin: user.isAdmin,
                 _id: user._id
-            }
-            res.send(temp)
+            };
+            res.send(temp);
         }
         else { return res.status(400).json({ message: 'Login failed!' }) }
     } catch (error) { return res.send(400).json({ error }) }
@@ -29,8 +29,8 @@ router.post('/login', async (req, res) => {
 
 router.get('/getallusers', async (req, res) => {
     try {
-        const users = await User.find()
-        res.send(users)
+        const users = await User.find();
+        res.send(users);
     } catch (error) { console.log(error) }
 })
 
--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-const dbConfig = require('./db')
+const dbConfig = require('./db');
 
-const roomsRoute = require('./routes/roomsRoute')
-const usersRoute = require('./routes/usersRoute')
-const bookingsRoute = require('./routes/bookingsRoute')
+const roomsRoute = require('./routes/roomsRoute');
+const usersRoute = require('./routes/usersRoute');
+const bookingsRoute = require('./routes/bookingsRoute');
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-app.use('/api/rooms', roomsRoute)
-app.use('/api/users', usersRoute)
-app.use('/api/bookings', bookingsRoute)
+app.use('/api/rooms', roomsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/bookings', bookingsRoute);
 
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port} 🔥`))
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
